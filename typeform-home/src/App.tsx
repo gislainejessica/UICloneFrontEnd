10b3cb149d14ba03221dd9fd21f25d5fc732cbc4
gislainejessica <gislainehjessica@gmail.com>
2020-09-16 13:38:14 -0300

    starting the project

---
+++ typeform-home/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import GlobalStyles from './styles/global'
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h2> Typeform </h2>
+    </>
+  );
+}
+
+export default App;
